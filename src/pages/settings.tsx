0161cbc045bd74e79f03b265505f44ad520e91b6
朱重迁 <z32664206862022@163.com>
2025-08-11 15:00:00 +0800

    feat(ui): 添加从剪贴板读取图片的功能
    在 `RouteComponent` 函数中引入 `useState` 来存储图片数据。
    添加 `onImageUpdate`, `startListening`, 和 `readImageBase64` 从 `tauri-plugin-clipboard-api` 的导入。
    在 `ls` 函数中，当监听到 "add-images" 事件时，尝试读取剪贴板中的图片并将其转换为 base64 格式。
    如果读取成功，则更新 `image` 状态变量。
    如果读取失败，则弹出警告提示用户当前剪贴板中没有图片。
    在返回的 JSX 中，如果 `image` 状态变量存在，则渲染该图片。

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,13 +1,17 @@
 import { Layout } from '@/layout'
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { createFileRoute } from '@tanstack/react-router'
 import { register, isRegistered, unregister } from '@tauri-apps/plugin-global-shortcut';
+import { onImageUpdate, startListening, readImageBase64 } from 'tauri-plugin-clipboard-api'
+
 import { listen } from '@tauri-apps/api/event';
 export const Route = createFileRoute('/settings')({
     component: RouteComponent,
 })
 
 function RouteComponent() {
+    const [image, setImage] = useState("")
+
     const init = async () => {
         const as = await isRegistered("ALT + T")
         // const asa = await unregister("ALT + T")
@@ -23,12 +27,26 @@ function RouteComponent() {
     const ls = () => {
         listen("add-images", async (event) => {
             console.log(event, '1==1=1=1==')
+            try {
+                const base64Img = await readImageBase64()
+                console.log(base64Img, '==1=1==1')
+                const image = `data:image/png;base64, ${base64Img}`
+                setImage(image)
+            } catch (e) {
+                alert("当前剪切板里面没有图片")
+            }
+
+            // .then((base64Img) => {
+            //   console.log(base64Img, '==1=1==1')
+            // const image = `data:image/png;base64, ${base64Img}`
+            // setImage(image)
         })
     }
 
     return <Layout>
         <div>
             <div className="p-2 border border-red-300 rounded-md">设置</div>
+            {image && <img src={image} alt="从剪贴板复制的图片" />}
         </div>
     </Layout>
 }
