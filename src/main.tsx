9ec00ee6d3a579ac52a4be10846f651cc228286b
朱重迁 <z32664206862022@163.com>
2025-08-14 16:33:55 +0800

    feat(desktop): 添加桌面通知功能
    在桌面环境中引入了 `@tauri-apps/plugin-notification` 插件，用于处理桌面通知。
    - 在 `bun.lock` 和 `package.json` 中添加了 `@tauri-apps/plugin-notification` 的依赖项。
    - 在 `src-tauri/Cargo.lock` 中添加了与通知功能相关的 Rust 包，包括 `mac-notification-sys` 和 `notify-rust`。
    - 在 `src-tauri/Cargo.toml` 中添加了 `tauri-plugin-notification` 的依赖项。
    - 在 `src-tauri/capabilities/desktop.json` 中添加了与通知相关的权限配置。
    - 在 `src-tauri/src/lib.rs` 中初始化了 `tauri-plugin-notification` 插件。
    - 在 `src/main.tsx` 中导入了与通知相关的函数，并在错误处理时实现了检查权限并发送通知的功能。

--- src/main.tsx
+++ src/main.tsx
@@ -12,15 +12,17 @@ import ReactDOM from "react-dom/client";
 import "./styles/global.css";
 
 import { routeTree } from './routeTree.gen';
-import { writeTextFile, BaseDirectory, exists, create, readTextFile, mkdir } from '@tauri-apps/plugin-fs';
 
-
+import {
+  isPermissionGranted,
+  requestPermission,
+  sendNotification,
+} from '@tauri-apps/plugin-notification';
 import { listen } from '@tauri-apps/api/event';
-import { uploadImageApi, useLoginApi, useUploadImageApi } from '@/api/auth'
+import { uploadImageApi, useLoginApi } from '@/api/auth'
 import { emitter } from '@/utils/mitt'
-import { onImageUpdate, startListening, readImageBase64, readImageBinary } from 'tauri-plugin-clipboard-api'
+import { readImageBinary } from 'tauri-plugin-clipboard-api'
 
-import { fetch } from '@tauri-apps/plugin-http';
 import { configs, initConfig } from './utils/config';
 import { loadStore } from './store';
 
@@ -43,7 +45,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 
 
 listen("add-images", async (event) => {
-  console.log(event, '1==1=1111=1121==')
   const store = (await loadStore())
 
   try {
@@ -52,26 +53,6 @@ listen("add-images", async (event) => {
     console.log(token, 'token')
 
     const ss = await readImageBinary('Blob') as Blob
-    // const formData = new FormData()
-    // formData.append('file', new Blob([ss], { type: 'image/png' }), 'image.png');
-    // const response = await fetch('https://api.chaoyang1024.top/api/storage', {
-    //   method: 'POST',
-    //   body: formData,
-    //   headers: {
-    //     Authorization: token ? `Bearer ${token}` : ''
-    //   }
-    // });
-    // const { code, message, data } = await response.json()
-    // if (code === 200) {
-    const data = {
-      "path": "https://vip.chaoyang1024.top/uploads/2025/8/14/image.png",
-      "title": "image.png",
-      "size": "104 KB",
-      "type": "image/png",
-      "created_at": "2025-08-14T01:33:41.361Z",
-      "storage_provider": "qiniu",
-      "id": 50
-    }
     if (token) {
 
       const data = await uploadImageApi(ss, token!)
@@ -79,8 +60,16 @@ listen("add-images", async (event) => {
       emitter.emit('add-image', data)
     }
   } catch (e) {
-    console.error(e, '-----')
-    // alert("当前剪切板里面没有图片")
+    let permissionGranted = await isPermissionGranted();
+
+    if (!permissionGranted) {
+      const permission = await requestPermission();
+      permissionGranted = permission === 'granted';
+    }
+
+    if (permissionGranted) {
+      sendNotification({ title: 'pasteboard', body: '当前剪切板里面没有图片' });
+    }
   }
 })
 // const
